08ec3d9748216c5eb84859305a989d4d456a876c
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-22 12:37:13 +0400

    SmmApp.java

--- src/SmmApp.java
+++ src/SmmApp.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class SmmApp {
+    public static void main(String[] args){
+        //int postCount = 101;
+        int likeCount=1400002; String postTitle="Mastering Java Fundamentals";
+        boolean isSponsored=true; double engagementRate=12.2;
+        System.out.println("Post Title: "+postTitle+" | Likes: "+likeCount+" | Engagement: "+engagementRate+"%"+" | Is Sponsored?: " + isSponsored);
+    }
 }
