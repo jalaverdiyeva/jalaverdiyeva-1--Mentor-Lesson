47ede0a88487ef4a71abc826b7257d6b488d3d97
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-22 12:34:48 +0400

    BankAnalogy.java

--- src/BankAnalogy.java
+++ src/BankAnalogy.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class BankAnalogy {
+    public static void main(String[] args) {
+        double balance=442444.4233; int reportedBalance=(int)balance;
+
+        System.out.println("Real Balance: "+balance+"\nReported Balance: "+reportedBalance);
+    }
 }
