f0656207e30ebab80687be59d162f9579c755c8c
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-22 12:35:56 +0400

    KassaHesablamasi.java

--- src/KassaHesablamasi.java
+++ src/KassaHesablamasi.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class KassaHesablamasi {
+    public static void main(String[] args){
+        float productPrice=250.50f; int discount=20;
+
+        float finalPrice=productPrice-(productPrice*discount/100.0f);
+
+        System.out.println("Original Price: "+productPrice+" | Discount: "+discount+"%"+" | Final Price: "+finalPrice);
+    }
 }
